99b1e51efd5d1e6bcdc72c709a4bef159bada1a4
Preet Parmar <preetparmar@outlook.com>
2021-12-08 15:53:13 +0530

    Day 02

---
+++ day_02/solution.py
@@ -0,0 +1,23 @@
+""" TESTING """
+input_data = '''forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2'''
+
+
+
+input_data
+
+
+
+
+
+
+
+
+
+# Importing data
+with open('./resources/day_01.txt') as input:
+    input_values = [int(x) for x in input.read().split()]
